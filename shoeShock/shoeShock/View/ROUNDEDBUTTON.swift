7959528b25bdf17bb73c16432b5f50115fffc0d0
Juan Torres <jmmtorres1991@yahoo.com>
2021-06-21 21:17:11 -0500

    Final Touches
    Definitely at its final form now.

---
+++ shoeShock/shoeShock/View/ROUNDEDBUTTON.swift
@@ -0,0 +1,16 @@
+//
+//  ROUNDEDBUTTON.swift
+//  shoeShock
+//
+//  Created by Juan Torres on 6/21/21.
+//
+
+import UIKit
+
+class ROUNDEDBUTTON: UIButton {
+
+    override  func awakeFromNib() {
+        layer.cornerRadius = 15
+    }
+
+}
